547369a1c004386814212b9f2555115057ef41bb
Cameron Cheung <cameronlcheung@yahoo.com>
2023-06-11 22:41:27 -0700

    Remove interops/c

--- interops/c/libUWAPI/include/UWAPI_helpers.h
+++
@@ -1,8 +0,0 @@
-#ifndef UWAPI_HELPERS_H
-#define UWAPI_HELPERS_H
-
-#include <UWAPI_plugin.h>
-
-void UWAPI_free_nonnull_position_stats(UWAPI_PositionStats *position_stats);
-
-#endif // UWAPI_HELPERS_H
